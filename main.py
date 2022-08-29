b164dc7613591f681fc08477fe43f3326d720a63
Alex Kneipp <alex@akneipp.com>
2022-08-29 17:10:35 -0400

    Adds parsing user input into an adjacency list

--- main.py
+++ main.py
@@ -9,11 +9,51 @@
 #Pitfalls to watch out for:
 # - Letters can't be used twice
 
+def get_input_bounding_box(lines):
+    rows = len(lines)
+    cols = len(lines[0])
+    for l in lines:
+        if len(lines) != cols:
+            print("All lines in input must have the same length as the bounding box of the puzzle")
+            return None
+    return(rows, cols)
+
 def parse_input(lines):
     """
-
+    :arg lines: List of input lines from the user which make up the puzzle
+    :returns: Graph (adj list) of the puzzle
     """
-    pass
+    row_max,col_max = get_input_bounding_box(lines)
+    G = {}
+    for row,line in enumerate(lines):
+        for col,c in enumerate(line):
+            G[(c,row,col)] = []
+
+            # Check to the left
+            if col-1 >= 0 and line[col-1] != ' ':
+                G[(c,row,col)].append((line[col-1],row,col-1))
+            # Check to the Right
+            if col+1 < col_max and line[col-1] != ' ':
+                G[(c,row,col)].append((line[col+1],row,col+1))
+            # Check above
+            if row-1 >= 0 and lines[row-1][col] != ' ':
+                G[(c,row,col)].append((lines[row-1][col],row-1,col))
+            # Check below
+            if row+1 < row_max and lines[row+1][col] != ' ':
+                G[(c,row,col)].append((lines[row+1][col],row+1,col))
+            # Check above left
+            if col-1 >= 0 and row-1 >= 0 and lines[row-1][col-1] != ' ':
+                G[(c,row,col)].append((lines[row-1][col-1],row-1,col-1))
+            # Check above right
+            if col+1 < col_max and row-1 >= 0 and lines[row-1][col+1] != ' ':
+                G[(c,row,col)].append((lines[row-1][col+1],row-1,col+1))
+            # Check below left
+            if col-1 >= 0 and row+1 < row_max and lines[row+1][col-1] != ' ':
+                G[(c,row,col)].append((lines[row+1][col-1],row+1,col-1))
+            # Check below right
+            if col+1 < col_max and row+1 < row_max and lines[row+1][col+1] != ' ':
+                G[(c,row,col)].append((lines[row+1][col+1],row+1,col+1))
+    return G
 
 def main():
     line = input()
@@ -21,9 +61,8 @@ def main():
     while line != "":
         lines.append(line)
         line = input()
-    print(lines)
-
-
+    G = parse_input(lines)
+    print(G)
 
 if __name__ == "__main__":
     main()
