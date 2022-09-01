b4d2a4ad05ac9b8997e6182e4eb3e80fa10740cf
Alex Kneipp <alex@akneipp.com>
2022-09-01 13:52:57 -0400

    A little bit more documentation

--- main.py
+++ main.py
@@ -10,6 +10,19 @@
 # - Letters can't be used twice
 
 def create_dict(dict_filename, min_len=0, max_len=100000, ignored_letters=''):
+    """
+    :arg dict_filename: The path to the dictionary file to read in.
+    The dictionary should be a lower-case, alphabetically ordered, newline separated list of words.
+    :arg min_len: The minimum length of words to consider.
+    Any words in the dictionary shorter than this will not be in the output.
+    :arg max_len: The maximum length of words to consider.
+    Similar in function to min_len.
+    :arg ignored_letters: String of letters which should be ignored.
+    Any words containing any of the letters in the string will not be in the output dictionary.
+    :return: A dictionary tree with the structure Tuple[<letter>, Dict<next letter>].
+    <letter> is the current letter in the tree dictionary, and the next letter dictionary is 
+    <letter>->Tuple[<letter>, Dict<nextLetter>] (i.e. it is recursively defined)
+    """
     root = ('', {})
     with open(dict_filename) as dict_file:
         for word in dict_file:
@@ -20,6 +33,7 @@ def create_dict(dict_filename, min_len=0, max_len=100000, ignored_letters=''):
 
 def add_word(dict_root, word, ignored_letters=''):
     if len(word) == 0:
+        dict_root[1][''] = ('', None)
         return True
     if word[0] in ignored_letters:
         return False
@@ -32,9 +46,8 @@ def add_word(dict_root, word, ignored_letters=''):
     return True
 
 def print_words(dict_root, so_far=''):
-    if len(dict_root[1]) == 0:
-        print(so_far,end='')
-        print(dict_root[0])
+    if len(dict_root[0]) == 0:
+        print(so_far)
     else:
         for e in dict_root[1]:
             print_words(dict_root[1][e], so_far+dict_root[0])
@@ -110,8 +123,36 @@ def get_ignored_letters(puzzle_graph):
             ig_letters.append(l)
     return ig_letters
 
+def get_step(letter, dict_node):
+    if letter in dict_node[1]:
+        return dict_node[1][letter]
+    return None
+
+def get_neighbors(n, puzzle_graph):
+    return puzzle_graph[n]
+
 def solve(puzzle_graph, dictionary):
-    pass
+    words = []
+    for n in puzzle_graph:
+        words.extend(get_words(n, [], dictionary, puzzle_graph))
+    return words
+
+def get_word_from_visited(visited):
+    w = []
+    for v in visited:
+        w.append(v[0])
+    return str(w)
+
+def get_words(start, visited, dict_node, puzzle_graph):
+    words = []
+    neighbors = get_neighbors(start, puzzle_graph)
+    if '' in dict_node[1]:
+        words.append(get_word_from_visited(visited));
+    for n in neighbors:
+        next_dict_step = get_step(n[0], dict_node)
+        if next_dict_step is not None and n not in visited:
+            words.extend(get_words(n, visited + [n], next_dict_step, puzzle_graph))
+    return words
 
 def main():
     line = input()
@@ -124,6 +165,7 @@ def main():
     print(get_puzzle_letters(G))
     words = create_dict('words_alpha.txt', min_len=4, ignored_letters=get_ignored_letters(G))
     print_words(words)
+    solve(G, words)
 
 if __name__ == "__main__":
     main()
